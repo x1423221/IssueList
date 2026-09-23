9ba7fc59f5d6cc38c98da0af0804368b29d0b6fc
unknown <I830@vdi.vghtc.gov.tw>
2026-09-23 08:45:43 +0800

    設定 base 為 IssueList

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/duty-issue/',   // 必須和 GitHub repo 名稱一致
+  base: '/IssueList/',   // 必須和 GitHub repo 名稱一致
   plugins: [vue()],
 })
